c239d5a6e75bffb0bd68879305c6a987d818b5b2
Brian Lobo <brianlobo.code@gmail.com>
2018-11-15 21:36:39 -0500

    Created a function that parses source code to find new listings and things leaving netflix

--- script.py
+++ script.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-source_code = requests.get('https://www.digitaltrends.com/home-theater/new-on-netflix/').text
-soup = BeautifulSoup(source_code, 'lxml')
-print(soup.prettify())
+def get_listings(url):
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code, 'lxml')
+    article = soup.find("article", class_="m-content").text
+    new_str = article.find('Everything new on Netflix')
+    leaving_str = article.find('Leaving Netflix')
+    new_listings = article[new_str : leaving_str]
+    leaving = article[leaving_str:]
+    return new_listings, leaving
+
+def main():
+    new_listings, leaving = get_listings('https://www.digitaltrends.com/home-theater/new-on-netflix/')
+
+if __name__ == '__main__':
+    main()
