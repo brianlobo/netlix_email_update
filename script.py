5aeb24619f3d87f09d1b3640a1e94f670a6a792b
Brian Lobo <brianlobo.code@gmail.com>
2018-11-15 22:28:53 -0500

    Will now send to email

--- script.py
+++ script.py
@@ -1,5 +1,30 @@
 from bs4 import BeautifulSoup
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import requests
+import smtplib
+
+
+def send_email(complete_str):
+    email = 'brianlobo.bot@gmail.com'
+    password = 'PASSWORD'
+    send_to_email = 'brianlobo.code@gmail.com'
+    subject = 'New Netflix Listings!'
+    message = complete_str
+
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = send_to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, send_to_email, text)
+    server.quit()
+    print("---Success---")
 
 def get_listings(url):
     source_code = requests.get(url).text
@@ -13,6 +38,8 @@ def get_listings(url):
 
 def main():
     new_listings, leaving = get_listings('https://www.digitaltrends.com/home-theater/new-on-netflix/')
+    complete_str = new_listings + '\n-------------\n' + leaving
+    send_email(complete_str)
 
 if __name__ == '__main__':
     main()
